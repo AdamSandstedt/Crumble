9bb23b28d87e6cd6b34524432de9e4d151e46785
Adam Sandstedt <adam.sandstedt@gmail.com>
2019-10-20 03:57:12 -0600

    Added more features to the graphics: check the description
    Added a line that previews the attempted split and a rectangle previewing the attempted  join. Most of the graphics are implemented but the game is still not functional

--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -3,6 +3,9 @@ package game;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -17,29 +20,33 @@ public class GameBoard extends JPanel {
 	private ArrayList<JButton> buttons;
 	private String currentAction;
 	private JFrame frame = new JFrame();
+	private Rectangle boardOutline;
+	private BoardPoint firstSelectionPoint, secondSelectionPoint;
+	private boolean showSplitLine;
+	private boolean showJoinRect;
+	private Point mousePosition;
+	private boolean splitDirection; // 0=vertical, 1=horizontal
 	
 	public static final Point TURN_TEXT_POSITION = new Point(650, 100);
 	
-	private ActionListener actionListener = new ActionListener() { 
+	private ActionListener buttonListener = new ActionListener() {
 		@Override
 		public void actionPerformed(ActionEvent e) { 
 			currentAction = e.getActionCommand();
-			if(currentAction.equals("split")) {
-				
-			}
-			else if(currentAction.equals("join")) {
-				
-			}
-			else if(currentAction.equals("swap")) {
-				
-			}
-			else if(currentAction.equals("end turn")) {
+			if(currentAction.equals("end turn")) {
 				currentTurn = !currentTurn;
 				currentAction = "split";
 				buttons.get(0).setEnabled(true);
 				buttons.get(1).setEnabled(true);
 				buttons.get(2).setEnabled(false);
 				buttons.get(3).setEnabled(false);
+				frame.repaint();
+			}
+			else if(currentAction.equals("split") || currentAction.equals("join")) {
+				firstSelectionPoint = null;
+				secondSelectionPoint = null;
+				showSplitLine = false;
+				showJoinRect = false;
 				frame.repaint();
 			}
 		}
@@ -54,6 +61,12 @@ public class GameBoard extends JPanel {
 	public void initialize() {
 		currentTurn = true;	// black goes first
 		currentAction = "split";
+		boardOutline = new Rectangle(GamePiece.X_OFFSET, GamePiece.Y_OFFSET, 600, 600);
+		firstSelectionPoint = null;
+		secondSelectionPoint = null;
+		showSplitLine = false;
+		showJoinRect = false;
+		splitDirection = false;
 		
 		GamePiece newPiece;
 		String location;
@@ -73,27 +86,27 @@ public class GameBoard extends JPanel {
 		JButton button = new JButton("Split");
 		button.setBounds(650, 200, 120, 50);
 		button.setActionCommand("split");
-		button.addActionListener(actionListener);
+		button.addActionListener(buttonListener);
 		buttons.add(button);
 		
 		button = new JButton("Join");
 		button.setBounds(650, 300, 120, 50);
 		button.setActionCommand("join");
-		button.addActionListener(actionListener);
+		button.addActionListener(buttonListener);
 		buttons.add(button);
 		
 		button = new JButton("Swap");
 		button.setBounds(650, 400, 120, 50);
 		button.setActionCommand("swap");
 		button.setEnabled(false);
-		button.addActionListener(actionListener);
+		button.addActionListener(buttonListener);
 		buttons.add(button);
 		
 		button = new JButton("End Turn");
 		button.setBounds(650, 500, 120, 50);
 		button.setActionCommand("end turn");
 		button.setEnabled(false);
-		button.addActionListener(actionListener);
+		button.addActionListener(buttonListener);
 		buttons.add(button);
 	}
 	
@@ -105,12 +118,36 @@ public class GameBoard extends JPanel {
 	}
 	
 	public void draw(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
 		String turnText;	//Set the turn text to the correct color and display it on the board
 		if(currentTurn) turnText = "Black's Turn";
 		else turnText = "White's Turn";
 		g.setColor(Color.black);
 		g.setFont(g.getFont().deriveFont((float)20));
 		g.drawString(turnText, TURN_TEXT_POSITION.x, TURN_TEXT_POSITION.y);
+		g.drawRect(boardOutline.x, boardOutline.y, boardOutline.width, boardOutline.height);
+		if(showSplitLine) {
+			int x1, y1, x2, y2;
+			x1 = firstSelectionPoint.toPoint().x;
+			y1 = firstSelectionPoint.toPoint().y;
+			BoardPoint p = getNearestPoint(new BoardPoint(mousePosition));
+			x2 = p.toPoint().x;
+			y2 = p.toPoint().y;
+			g2.setColor(Color.red);
+			g2.setStroke(new BasicStroke(4));
+			g2.drawLine(x1, y1, x2, y2);
+		}
+		if(showJoinRect) {
+			int x1, y1, width, height;
+			BoardPoint p = getNearestPoint(new BoardPoint(mousePosition));
+			x1 = firstSelectionPoint.toPoint().x;
+			y1 = p.toPoint().y;
+			width = p.toPoint().x - x1;
+			height = firstSelectionPoint.toPoint().y - y1;
+			g2.setColor(Color.red);
+			g2.setStroke(new BasicStroke(4));
+			g2.drawRect(x1, y1, width, height);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -125,6 +162,174 @@ public class GameBoard extends JPanel {
 			board.frame.add(button);
 		}
 		
+		board.frame.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Point p = e.getPoint();
+				p.translate(0, -22);
+				if(board.boardOutline.contains(p)) {
+					BoardPoint click = new BoardPoint(p);
+					if(board.currentAction.equals("split") || board.currentAction.equals("join")) {
+						if(board.firstSelectionPoint == null) {
+							board.firstSelectionPoint = board.getNearestPoint(click);
+						}
+						else {
+							board.secondSelectionPoint = board.getNearestPoint(click);
+							if(board.currentAction.equals("split")) board.split();
+							else board.join();
+						}
+					}
+					else if(board.currentAction.equals("swap")) {
+						
+					}
+				}
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+
+			@Override
+			public void mouseExited(MouseEvent e) {}
+		});
+		
+		board.frame.addMouseMotionListener(new MouseMotionListener() {
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				if(board.firstSelectionPoint != null) {
+					if(board.currentAction.equals("split")) {
+						board.mousePosition = e.getPoint();
+						board.mousePosition.translate(0, -22);
+						board.showSplitLine = true;
+						board.frame.repaint();
+					}
+					else if(board.currentAction.equals("join")) {
+						board.mousePosition = e.getPoint();
+						board.mousePosition.translate(0, -22);
+						board.showJoinRect = true;
+						board.frame.repaint();
+					}
+				}
+				
+			}
+			
+		});
+	}
+
+	public void join() {
+		showJoinRect = false;
+		currentAction = "swap";
+		buttons.get(0).setEnabled(false);
+		buttons.get(1).setEnabled(false);
+		buttons.get(2).setEnabled(true);
+		buttons.get(3).setEnabled(true);
+		firstSelectionPoint = null;
+		secondSelectionPoint = null;
+		frame.repaint();
+	}
+
+	public void split() {
+		showSplitLine = false;
+		currentAction = "swap";
+		buttons.get(0).setEnabled(false);
+		buttons.get(1).setEnabled(false);
+		buttons.get(2).setEnabled(true);
+		buttons.get(3).setEnabled(true);
+		firstSelectionPoint = null;
+		secondSelectionPoint = null;
+		frame.repaint();
+	}
+	
+	public BoardPoint getNearestPoint(BoardPoint point) {
+		double minSquareDistance = 100;
+		BoardPoint nearestPoint = null;
+		if(firstSelectionPoint == null) {
+			secondSelectionPoint = null;
+			switch(currentAction) {
+			case "split":
+				for(GamePiece piece: gamePieces) {
+					if(piece.isColor() == currentTurn) {
+						if(point.distanceSq(piece.getHorizontalSplitStart()) < minSquareDistance) {
+							splitDirection = true;
+							nearestPoint = piece.getHorizontalSplitStart();
+							minSquareDistance = point.distanceSq(piece.getHorizontalSplitStart());
+						}
+						if(point.distanceSq(piece.getVerticalSplitStart()) < minSquareDistance) {
+							splitDirection = false;
+							nearestPoint = piece.getVerticalSplitStart();
+							minSquareDistance = point.distanceSq(piece.getVerticalSplitStart());
+						}
+					}
+				}
+				break;
+			case "join":
+				for(GamePiece piece: gamePieces) {
+					if(piece.isColor() == currentTurn) {
+						if(point.distanceSq(piece.getBottomLeft()) < minSquareDistance) {
+							nearestPoint = piece.getBottomLeft();
+							minSquareDistance = point.distanceSq(piece.getBottomLeft());
+						}
+						if(point.distanceSq(piece.getBottomLeft()) < minSquareDistance) {
+							nearestPoint = piece.getBottomLeft();
+							minSquareDistance = point.distanceSq(piece.getBottomLeft());
+						}
+					}
+				}
+				break;
+			}
+		}
+		else {
+			switch(currentAction) {
+			case "split":
+				for(GamePiece piece: gamePieces) {
+					if(piece.isColor() == currentTurn) {
+						if(splitDirection) {
+							BoardPoint end = piece.getHorizontalSplitEnd();
+							if(end.getY() == firstSelectionPoint.getY() && end.getX() > firstSelectionPoint.getX())
+								if(point.distanceSq(end) < minSquareDistance) {
+									nearestPoint = end;
+									minSquareDistance = point.distanceSq(end);
+								}
+						}
+						else {
+							BoardPoint end = piece.getVerticalSplitEnd();
+							if(end.getX() == firstSelectionPoint.getX() && end.getY() > firstSelectionPoint.getY())
+								if(point.distanceSq(end) < minSquareDistance) {
+									nearestPoint = end;
+									minSquareDistance = point.distanceSq(end);
+								}
+						}
+							
+					}
+				}
+				break;
+			case "join":
+				for(GamePiece piece: gamePieces) {
+					if(piece.isColor() == currentTurn) {
+						if(piece.getTopRight().getX() > firstSelectionPoint.getX() && piece.getTopRight().getY() > firstSelectionPoint.getY()) {
+							if(point.distanceSq(piece.getTopRight()) < minSquareDistance) {
+								nearestPoint = piece.getTopRight();
+								minSquareDistance = point.distanceSq(piece.getTopRight());
+							}
+						}
+					}
+				}
+				break;
+			}
+		}
+		return nearestPoint;
 	}
 	
 }
