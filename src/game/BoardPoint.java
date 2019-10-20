dd064d680b6af48a24a06faade84373dd9639f67
Adam Sandstedt <adam.sandstedt@gmail.com>
2019-10-20 03:50:30 -0600

    added a BoardPoint constructor from a Point and implemented a squared distance method
    The squared distance takes another BoardPoint, and returns the square of the distances between it and the original

--- src/game/BoardPoint.java
+++ src/game/BoardPoint.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.awt.Point;
+
 public class BoardPoint {
 	private double x;
 	private double y;
@@ -7,6 +9,11 @@ public class BoardPoint {
 	BoardPoint(double x, double y) {
 		this.x = x;
 		this.y = y;
+	}
+	
+	BoardPoint(Point p) {
+		this.x = ((double)p.x - GamePiece.X_OFFSET)/100;
+		this.y = ((double)GamePiece.Y_OFFSET + 600 - p.y)/100;
 	}
 
 	public double getX() {
@@ -25,6 +32,16 @@ public class BoardPoint {
 	public boolean equals(BoardPoint p) {
 		if(this.x == p.getX() && this.y == p.getY()) return true;
 		else return false;
+	}
+	
+	public double distanceSq(BoardPoint p) {
+		double dx = p.x - this.x;
+		double dy = p.y - this.y;
+		return dx*dx + dy*dy;
+	}
+	
+	public Point toPoint() {
+		return new Point((int)(GamePiece.X_OFFSET+100*x), (int)(600+GamePiece.Y_OFFSET-100*y));
 	}
 	
 }
